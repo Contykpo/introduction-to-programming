d46640f4a95818d776c405c234d926afb9d0bdea
Contykpo <samu.1furlanich@gmail.com>
2023-04-28 20:25:25 -0300

    Ver Descripcion
    - Otra definicion de la funcion "partes" en el Ejercicio 6, Guia 5.

--- Guia 5/Ejercicio6.hs
+++ Guia 5/Ejercicio6.hs
@@ -6,13 +6,31 @@ agregarATodos _ [] = []
 agregarATodos n (x:xs) = if pertenece n x then agregarATodos n xs else (n : x) : agregarATodos n xs
 
 -- B
+{-
 partes :: [Integer] -> [Set]
 partes []  = [[]]
 partes (x:xs) = partes xs ++ map (x:) (partes xs)
+-}
+
+partes :: Integer -> [Set]
+partes 0 = [[],[1]]
+partes n = agregarTodos (partes (n-1)) n ++ partes (n-1)
+
+agregarTodos :: [[Integer]] -> Integer -> [[Integer]]
+agregarTodos [] _ = []
+agregarTodos (xs:xss) y = (y : xs) : agregarTodos xss y
 
 -- C
-productoCartesiano :: Set -> Set -> [(Integer, Integer)]
-productoCartesiano xs ys = [(x,y) | x <- xs, y <- ys]
+--productoCartesiano :: Set -> Set -> [(Integer, Integer)]
+--productoCartesiano xs ys = [(x,y) | x <- xs, y <- ys]
+
+productoCartesiano :: Set -> Set -> [(Integer,Integer)]
+productoCartesiano [] ys = []
+productoCartesiano (a:b) ys = (productoCartesiano' a ys) ++ (productoCartesiano b ys)
+
+productoCartesiano' :: Integer -> Set -> [(Integer,Integer)]
+productoCartesiano' x [] = []
+productoCartesiano' x (a:b) = (x, a) : (productoCartesiano' x b)
 
 -- Helper Functions --
 

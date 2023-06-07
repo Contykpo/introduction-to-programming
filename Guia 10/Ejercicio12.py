16d0d4443839e4f9534e6b6e4716fd6fffd71511
Contykpo <samu.1furlanich@gmail.com>
2023-06-07 14:22:56 -0300

    Update Ejercicio12.py

--- Guia 10/Ejercicio12.py
+++ Guia 10/Ejercicio12.py
@@ -17,9 +17,26 @@ def estaBienBalanceada(formula: str) -> bool:
             parentesisCerrados += 1
         if parentesisCerrados >= 1 and caracterOperando == "" and formulaSinEspacios[iterador] == formulaSinEspacios[len(formulaSinEspacios)-1]:
             return False
-        if parentesisAbiertos < parentesisCerrados:
+        if not parentesisAbiertos == parentesisCerrados:
             return False
     return parentesisAbiertos == parentesisCerrados
 
 
-print(str(estaBienBalanceada("1 + )2 * 3(")))
+def esta_bien_balanceada(formula: str) -> bool:
+    formulaSinEspacios: str = formula.replace(" ", "")
+    parentesisAbiertos: int = 0
+    parentesisCerrados: int = 0
+    operadores: List[str] = ["+","-","*","/"]
+    for iterador in range(0,len(formulaSinEspacios),1):
+        if formulaSinEspacios[iterador] == "(":
+            parentesisAbiertos += 1
+            if iterador != 0 and (not formulaSinEspacios[iterador - 1] in operadores):
+                return False
+        if formulaSinEspacios[iterador] == ")":
+            parentesisCerrados += 1
+            if iterador != (len(formulaSinEspacios)-1) and (not formulaSinEspacios[iterador + 1] in operadores):
+                return False
+    return parentesisAbiertos == parentesisCerrados
+
+
+print(str(esta_bien_balanceada("(1) + 2 * 3")))
